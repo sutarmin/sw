295293d57ac734fe1d4cda4197c675d553768aa4
Anton Sutarmin <sutarmin.aa@gmail.com>
2016-12-06 22:03:32 +0300

    Методы сегментации в scikit

--- src/main.py
+++ src/main.py
@@ -5,12 +5,16 @@ from matplotlib import pyplot as plt
 import skimage
 from skimage import exposure, morphology, color
 
+from skimage.segmentation import quickshift
+from skimage.segmentation import mark_boundaries
+
 from ImageRepository import ImageRepository as Repo
 from MultiImageViewer import imagebuilder as ib
 
 from utils import process, process_yield, view
 from history import effort2
 from handlers import quantize, normalize_hist, get_channel
+
 
 
 def effort_cur(img):
@@ -23,12 +27,20 @@ if __name__ == '__main__':
     # first = cv2.cvtColor(first, cv2.COLOR_RGB2BGR)
     # second = cv2.cvtColor(second, cv2.COLOR_RGB2BGR)
 
-    func_arr = effort2('r')
-    for i, f in enumerate(process_yield(first, func_arr)):
-        ib.draw(f, (2, len(func_arr)), (1, i + 1))
-    for i, f in enumerate(process_yield(second, func_arr)):
-        ib.draw(f, (2, len(func_arr)), (2, i + 1))
-    ib.show()
+    quickshift_segments1 = quickshift(first, kernel_size=3, max_dist=6, ratio=0.5)
+    quickshift_segments2 = quickshift(first, ratio=0.5)
+
+    mark1 = mark_boundaries(first, quickshift_segments1)
+    mark2 = mark_boundaries(first, quickshift_segments2)
+
+    view(mark2 - mark1)
+
+    # func_arr = effort2('r')
+    # for i, f in enumerate(process_yield(first, func_arr)):
+    #     ib.draw(f, (2, len(func_arr)), (1, i + 1))
+    # for i, f in enumerate(process_yield(second, func_arr)):
+    #     ib.draw(f, (2, len(func_arr)), (2, i + 1))
+    # ib.show()
 
     # img = process(first, effort2('b'))
 
