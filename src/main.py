6e021dfcae30252f68a129e96b42a1d975ad7454
sutarmin <sutarmin.aa@gmail.com>
2016-12-07 22:44:08 +0300

    готовимся кэшировать результаты quickshift

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ import matplotlib
 from matplotlib import pyplot as plt
 import skimage
 from skimage import exposure, morphology, color
-
+import time
+import pickle
 from skimage.segmentation import quickshift
 from skimage.segmentation import mark_boundaries
 
@@ -16,7 +17,6 @@ from history import effort2
 from handlers import quantize, normalize_hist, get_channel
 
 
-
 def effort_cur(img):
     img = quantize(img, 5)
     img = normalize_hist(img)
@@ -24,24 +24,21 @@ def effort_cur(img):
 
 if __name__ == '__main__':
     first, second = Repo.get_set(0)
-    # first = cv2.cvtColor(first, cv2.COLOR_RGB2BGR)
+    first = cv2.cvtColor(first, cv2.COLOR_RGB2BGR)
     # second = cv2.cvtColor(second, cv2.COLOR_RGB2BGR)
 
+
+
+    start = time.process_time()
+    print("start")
     quickshift_segments1 = quickshift(first, kernel_size=3, max_dist=6, ratio=0.5)
-    quickshift_segments2 = quickshift(first, ratio=0.5)
+    # quickshift_segments2 = quickshift(first, ratio=0.5)
+    print("done. caching")
+    with open('../img/dumps/quickshift/0_left.pkl', 'wb') as f:
+        pickle.dump(quickshift_segments1, f)
+
+    print(time.process_time() - start)
 
     mark1 = mark_boundaries(first, quickshift_segments1)
-    mark2 = mark_boundaries(first, quickshift_segments2)
-
-    view(mark2 - mark1)
-
-    # func_arr = effort2('r')
-    # for i, f in enumerate(process_yield(first, func_arr)):
-    #     ib.draw(f, (2, len(func_arr)), (1, i + 1))
-    # for i, f in enumerate(process_yield(second, func_arr)):
-    #     ib.draw(f, (2, len(func_arr)), (2, i + 1))
-    # ib.show()
-
-    # img = process(first, effort2('b'))
 
     cv2.destroyAllWindows()
