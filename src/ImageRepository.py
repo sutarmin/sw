4313875feac16aeaf1f7c2b543ff49feb106948a
Anton Sutarmin <sutarmin.aa@gmail.com>
2016-12-12 08:01:16 +0300

    у5н

--- src/ImageRepository.py
+++ src/ImageRepository.py
@@ -1,31 +1,50 @@
 import pickle
-
+import numpy as np
 import cv2
 
 
-image_source = "..\\img\\"
-
-
 class ImageRepository:
-    dump_folder = '../img/dumps/'
+    image_source = '../img/'
 
     @staticmethod
     def get_set(set_num):
-        first = cv2.imread(image_source + str(set_num) + "_left.jpg")
-        second = cv2.imread(image_source + str(set_num) + "_right.jpg")
+        first = cv2.imread(ImageRepository.image_source + str(set_num) + "_left.jpg")
+        second = cv2.imread(ImageRepository.image_source + str(set_num) + "_right.jpg")
         return first, second
 
     @staticmethod
     def get_img(img_name):
-        img = cv2.imread(image_source + img_name)
+        img = cv2.imread(ImageRepository.image_source + img_name)
         return img
 
     @staticmethod
-    def dump(obj, file):
-        with open(ImageRepository.dump_folder + file + '.pkl', "wb") as f:
-            pickle.dump(obj, f)
+    def dump(obj: np.ndarray, filename: str):
+        if obj.dtype != 'int64':
+            raise TypeError('I can only dump results of quickshift segmentation!')
+
+        res = np.zeros((obj.shape[0], obj.shape[1], 3), np.uint8)
+        for x in range(obj.shape[0]):
+            for y in range(obj.shape[1]):
+                val = obj.item((x, y))
+                r_val = val // (256 ** 2)
+                g_val = (val % (256 ** 2)) // 256
+                b_val = (val % (256 ** 2)) % 256
+                res.itemset((x, y, 0), r_val)
+                res.itemset((x, y, 1), g_val)
+                res.itemset((x, y, 2), b_val)
+
+        path = ImageRepository.image_source + filename + '.png'
+        cv2.imwrite(path, res)
 
     @staticmethod
-    def from_dump(file):
-        with open(ImageRepository.dump_folder + file + '.pkl', "rb") as f:
-            return pickle.load(f)
+    def from_dump(filename: str):
+        dump = cv2.imread(ImageRepository.image_source + filename + '.png')
+        res = np.zeros((dump.shape[0], dump.shape[1]), np.int64)
+        for x in range(dump.shape[0]):
+            for y in range(dump.shape[1]):
+                val = dump.item((x, y, 2))
+                val += dump.item((x, y, 1)) * 256
+                val += dump.item((x, y, 0)) * 256 ** 2
+                res.itemset((x, y), val)
+
+        return res
--- src/main.py
+++ src/main.py
@@ -1,19 +1,6 @@
-import numpy as np
-import cv2
-import matplotlib
-from matplotlib import pyplot as plt
-import skimage
-from skimage import exposure, morphology, color
-import time
-import pickle
 from skimage.segmentation import quickshift
-from skimage.segmentation import mark_boundaries
 
 from ImageRepository import ImageRepository as Repo
-from MultiImageViewer import imagebuilder as ib
-
-from utils import process, process_yield, view
-from history import effort2
 from handlers import quantize, normalize_hist, get_channel
 
 
@@ -22,35 +9,14 @@ def effort_cur(img):
     img = normalize_hist(img)
 
 
-def temp_not_main():
-    first, second = Repo.get_set(0)
-    first = cv2.cvtColor(first, cv2.COLOR_RGB2BGR)
-    # second = cv2.cvtColor(second, cv2.COLOR_RGB2BGR)
-
-
-
-    start = time.process_time()
-    print("start")
-    quickshift_segments1 = quickshift(first, kernel_size=3, max_dist=6, ratio=0.5)
-    # quickshift_segments2 = quickshift(first, ratio=0.5)
-    print("done. caching")
-    with open('../img/dumps/quickshift/0_left.pkl', 'wb') as f:
-        pickle.dump(quickshift_segments1, f)
-
-    print(time.process_time() - start)
-
-    mark1 = mark_boundaries(first, quickshift_segments1)
-
-    cv2.destroyAllWindows()
-
-
 def temp_main():
-    first, second = Repo.get_set(0)
-    print("Start")
-    quickshift_seg = quickshift(second, kernel_size=3, max_dist=6, ratio=0.5)
-    print("Done")
-    Repo.dump(quickshift_seg, 'quickshift/0_right')
+    for i in range(3):
+        print('Set ' + str(i))
+        left, right = Repo.get_set(i)
+        seg = quickshift(left, kernel_size=3, max_dist=6, ratio=0.5)
+        Repo.dump(seg, 'quickshift/' + str(i) + '_left')
+        seg = quickshift(right, kernel_size=3, max_dist=6, ratio=0.5)
+        Repo.dump(seg, 'quickshift/' + str(i) + '_right')
 
-
-if __name__ == '__main__':
+if ё__name__ == '__main__':
     temp_main()
